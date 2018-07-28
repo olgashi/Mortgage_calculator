d39f9223451b0cc8154199d01ad4943aece054c6
Oshi99 <oshiryaeva@protonmail.com>
2018-07-28 16:51:15 -0500

    added yml messages file

--- mortgage_calculator.rb
+++ mortgage_calculator.rb
@@ -1,3 +1,6 @@
+require 'yaml'
+MESSAGES = YAML.load_file('mortgage_calculator_messages.yml')
+
 def integer?(num)
   num.to_i.to_s == num
 end
@@ -10,45 +13,42 @@ def valid_number?(num)
   integer?(num) || float?(num)
 end
 
-def maturity_years_to_months(months)
-  months * 12
-end
-
-def monthly_apr(interest)
-  interest / 1200
-end
-
 def monthly_payment(p, j, n)
-
-  m = p.to_f * (monthly_apr(j.to_f) / (1 - (1 + monthly_apr(j.to_f))**(-maturity_years_to_months(n.to_i))))
+  p * (j / (1 - (1 + j)**-n))
 end
+
+def prompt(message)
+  puts("=> #{message}")
+end
+
+prompt(MESSAGES['welcome'])
 
 loop do # main loop
   principal = ''
   apr = ''
   n = ''
   loop do
-    puts "Enter loan amount. Please provide whole dollar amount (for example, $130500)."
+    prompt(MESSAGES['principal'])
     principal = gets.chomp
     if integer?(principal)
       break
     else
-      puts "Please enter a valid amount."
+      prompt(MESSAGES['valid_amount'])
     end
   end
 
   loop do
-    puts "Enter Annual Percentage Rate without '%' sign, (for example, if 5.5%, then enter 5.5): "
+    prompt(MESSAGES['apr'])
     apr = gets.chomp
     if valid_number?(apr)
       break
     else
-      puts "Please enter a valid amount."
+      prompt(MESSAGES['valid_amount'])
     end
   end
-  
+
   loop do
-    puts "Enter loan duration (maturity) as a whole number, in years: "
+    prompt(MESSAGES['maturity'])
     n = gets.chomp
     if integer?(n)
       break
@@ -56,11 +56,16 @@ loop do # main loop
       puts "Please enter a valid amount."
     end
   end
-  
-  puts (monthly_payment(principal, apr, n)).round(2)
 
+  principal = principal.to_f
+  apr = apr.to_f / 1200
+  n = n.to_i * 12
 
-  puts "Do you want to repeat? Enter 'y' for yes, any other key for no."
+  puts "Monthly payment is: $#{monthly_payment(principal, apr, n).round(2)}"
+
+  prompt(MESSAGES['repeat'])
   answer = gets().chomp()
-  break unless answer.downcase().start_with?('y')      
+  break unless answer.downcase().start_with?('y')
 end
+
+prompt(MESSAGES['goodbye'])
